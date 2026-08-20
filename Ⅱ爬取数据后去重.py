90ddfbc087dfeafa4125c640fea201298e80575f
pengzlong3-arch <pengzlong3@gmail.com>
2026-08-20 08:08:58 +0800

    #8.20提交,ai辅助爬取加评分

--- Ⅱ爬取数据后去重.py
+++ Ⅱ爬取数据后去重.py
@@ -2,7 +2,7 @@ from tkinter.constants import FIRST
 
 import pandas as pd
 
-df = pd.read_csv('data/八字自动录入数据.csv', sep=',')
+df = pd.read_excel('data/八字自动录入数据.xlsx')
 df.drop_duplicates(inplace=True,ignore_index=True,subset='评语',keep='first')    #评语一致的才删掉,保留最先出现的
 print(df)
 df.to_csv('data/八字自动录入数据.csv', sep=',',header=True, index= False)
--- Ⅵxgboost.py
+++ Ⅵxgboost.py
@@ -15,23 +15,24 @@ from sklearn.utils import class_weight                           #权重平衡
 
 def xgboosting():
     # 1.加载数据
-    csv = pd.read_csv('data/八字数据.csv', sep=',', usecols=['天干1', '地支1', '天干2', '地支2', '天干3', '地支3', '天干4', '地支4', '性别', '得分'])
+    csv = pd.read_csv('data/八字自动录入数据(AI评分).csv', sep=',', usecols=['天干1', '地支1', '天干2', '地支2', '天干3', '地支3', '天干4', '地支4', '性别', 'AI评分'])
     # 2.数据预处理
-    data = csv.query('得分 not in ["争议",0,1,2,3,12,11,10,9]').loc[:, '天干1':'性别']
-    target = csv.query('得分 not in ["争议",0,1,2,3,12,11,10,9]').loc[:, '得分'] - 4    #减去4,从0-4评分,不然会报错
+    # data = csv.query('得分 not in ["争议",0,1,2,3,12,11,10,9]').loc[:, '天干1':'性别']
+    data = csv.loc[:, '天干1':'性别']
+    # target = csv.query('得分 not in ["争议",0,1,2,3,12,11,10,9]').loc[:, '得分'] - 4    #减去4,从0-4评分,不然会报错
+    target = csv.loc[:, 'AI评分']    #减去4,从0-4评分,不然会报错
     # 3.特征工程
     # 3.1分类特征热编码(xgboost不用进行热编码)
     # data = pd.get_dummies(data,columns=['天干1', '地支1', '天干2', '地支2', '天干3', '地支3', '天干4', '地支4', '性别'],drop_first=True)  # drop_first删掉一个冗余的列
     # 3.2切分测试集训练集
-    x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=11, stratify=target)
+    x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=9, stratify=target)
     # 4.模型训练
     sw = class_weight.compute_sample_weight('balanced', y_train)   #平衡权重
     estimator = xgb.XGBClassifier(
         enable_categorical=True,
-        max_depth=5,                # 树的最大深度
+        max_depth=3,                # 树的最大深度
         n_estimators=120,           # 树的数量
         learning_rate=0.01,          # 学习率
-        random_state=23,            # 随机种子
         objective='multi:softmax'   # 多分类问题, 使用多分类模型.
     )
     estimator.fit(x_train, y_train,sample_weight=sw)
